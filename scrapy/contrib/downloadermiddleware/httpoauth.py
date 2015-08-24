a534521d40aebf81a55dd69477dabc0b221e3e96
Josh Levy-Kramer <josh@starcount.com>
2015-08-24 18:44:20 +0100

    Simplified request rescheduling method

--- scrapy/contrib/downloadermiddleware/httpoauth.py
+++ scrapy/contrib/downloadermiddleware/httpoauth.py
@@ -4,11 +4,11 @@ HTTP OAuth downloader middleware
 See documentation in docs/topics/downloader-middleware.rst
 """
 from collections import deque
-from time import time, sleep
+from time import time
 from oauthlib.oauth1 import Client as Oauth1Client
 from oauthlib.oauth2 import InsecureTransportError
 from oauthlib.oauth2 import WebApplicationClient as Oauth2Client
-from scrapy.exceptions import DontCloseSpider, NotConfigured, IgnoreRequest
+from scrapy.exceptions import NotConfigured
 
 from scrapy import signals
 
@@ -20,14 +20,13 @@ class HttpOAuth1Middleware(object):
     def from_crawler(cls, crawler):
         o = cls()
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        crawler.signals.connect(o.spider_idle, signal=signals.spider_idle)
         return o
 
     def spider_opened(self, spider):
         # 'tokens' is a list of dictionaries which contain:
         # oauth_client_key, oauth_client_secret, oauth_resource_owner_key, oauth_resource_owner_secret
         tokens = getattr(spider, 'oauth_token_list', None)
-        self.check_response  = getattr(spider, 'oauth_check_response_func', self.default_check_response)
+        self.check_response = getattr(spider, 'oauth_check_response_func', self.default_check_response)
         self.REQUEST_WINDOW_SIZE_MINS = getattr(spider, 'oauth_request_windows_size_mins', 0)
 
         if tokens is None:
@@ -38,16 +37,12 @@ class HttpOAuth1Middleware(object):
         # All tokens start off in dead deque. Contains tuples: (token, time_died)
         self.tokens_dead = deque(zip(tokens, [float('-inf')] * len(tokens)))
 
-        self.requests_on_hold = []
-
     def process_request(self, request, spider):
         # Obtain token
         token, requests_done = self._obtain_token(spider)
 
         if token is None:
-            # Add to requests on hold list and do request later
-            self.requests_on_hold.append(request)
-            raise IgnoreRequest
+            return request
 
         auth = Oauth1Client(
             client_key=token['client_key'],
@@ -56,14 +51,13 @@ class HttpOAuth1Middleware(object):
             resource_owner_secret=token['resource_owner_secret'])
 
         uri, headers, body = auth.sign(request.url)
-        #request = request.replace(headers=headers)
         request.headers['Authorization'] = [headers['Authorization']]
 
         request.meta['oauth'] = True
         request.meta['token'] = token
 
     def process_response(self, request, response, spider):
-        ''' Change token if required and retry if necessary '''
+        """ Change token if required and retry if necessary """
 
         oauth_used = request.meta.get('oauth', False)
 
@@ -79,7 +73,7 @@ class HttpOAuth1Middleware(object):
                 requests_succeed += 1
                 self.tokens_live.append((token, requests_succeed))
 
-            if retry_request == True:
+            if retry_request:
                 # Reschedule request but remove the token
                 del request.meta['token']
                 del request.meta['oauth']
@@ -88,42 +82,16 @@ class HttpOAuth1Middleware(object):
                 # Request success
                 return response
 
-    def spider_idle(self, spider):
-        '''
-        The spider has gone idle, which means the spider has no further:
-        * requests waiting to be downloaded
-        * requests scheduled
-        * items being processed in the item pipeline
-        '''
-
-        if len(self.requests_on_hold) != 0:
-            # Use live tokens if some remain
-            if len(self.tokens_live) == 0:
-
-                # How much time left until we can reuse the dead tokens
-                time_left = self._dead_token_time_left()
-
-                # If still need to wait then sleep
-                if time_left > 0:
-                    sleep(time_left)        # TODO: need to change
-
-            # Reschedule requests with the spider
-            for r in self.requests_on_hold:
-                spider.crawler.engine.crawl(r, spider)
-
-            self.requests_on_hold = []  # Clear request on hold
-            raise DontCloseSpider()  # Stop the spider from closing
-
     def default_check_response(self, response):
         # By default just cycle through all the tokens
         # return: token_dead, retry_request
         return True, False
 
     def _dead_token_time_left(self):
-        '''
+        """
         Returns the number of seconds left until the first dead tokens can be used again.
         If there are no dead tokens return infinity
-        '''
+        """
         if len(self.tokens_dead) > 0:
             cred, time_expired = self.tokens_dead[0]  # Peek at the beginning of the queue
             time_left = self.REQUEST_WINDOW_SIZE_MINS * 60 - (time() - time_expired)
@@ -133,7 +101,7 @@ class HttpOAuth1Middleware(object):
             return float('inf')
 
     def _obtain_token(self, spider):
-        ''' Obtain a token from the live or dead deque. Returns None if no current tokens '''
+        """ Obtain a token from the live or dead deque. Returns None if no current tokens """
 
         # Check to see if there there any tokens available to use
         if len(self.tokens_live) == 0 and self._dead_token_time_left() > 0:
